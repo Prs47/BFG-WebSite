260c9fe3b51de8a7a5dec154359c530248a5b753
bamfg110 <bamfg110@gmail.com>
2025-10-04 16:31:56 +0330

    1

--- BFGWebSite/settings.py
+++ BFGWebSite/settings.py
@@ -196,7 +196,7 @@ CELERY_BEAT_SCHEDULE = {
 
 EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')
 EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_PORT = int(os.getenv('EMAIL_PORT'))
+EMAIL_PORT = int(os.getenv('EMAIL_PORT') or 587)
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'False').lower() in ('1','true','yes')
